3927a38fe4cd89be9cc87117efa571e6ee01e0e5
Kenan Memmedli <mrkonsol@mail.ru>
2026-04-18 18:29:08 +0800

    music service fix

--- beatlybackend/beatly.API/obj/Debug/net8.0/beatly.API.AssemblyInfo.cs
+++ beatlybackend/beatly.API/obj/Debug/net8.0/beatly.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beatly.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b937c250006aadbda00e9e40046b7b947948d41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("beatly.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beatly.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
